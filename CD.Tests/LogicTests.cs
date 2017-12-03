fcd0116659475b16ac0c831b1dff7ca5cd129a7b
Brandon Nicoll <bgnicoll@gmail.com>
2017-12-02 21:21:05 -0600

    Comprehensive suite of tests

---
+++ CD.Tests/LogicTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace CD.Tests
+{
+    [TestFixture]
+    public class LogicTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
